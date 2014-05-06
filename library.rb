18e075fd5e6ca3f7274acb4579cf857e6899ddfa
Greg Cosgrove <gregcosgrove@gmail.com>
2014-05-06 17:51:19 -0500

    Refactor check-in check-out methods

--- library.rb
+++ library.rb
@@ -4,25 +4,25 @@ class Book
   attr_reader :title, :author, :status
   attr_accessor :id
 
+
+
   def initialize(title=nil, author=nil)
     @title = title
     @author = author
-    @id = nil
     @status = 'available'
   end
 
   def check_out
-    if @status == 'available'
-      @status = 'checked_out'
+    if @status == "available"
+      @status = "checked_out"
       true
-    elsif @status == 'checked_out'
+    else
       false
     end
   end
 
   def check_in
-    if @status == 'checked_out'
-      @status = 'available'
+      @status = "available"
     end
   end
 
